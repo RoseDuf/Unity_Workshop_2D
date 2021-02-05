88882f3935ea7ab9c92f279bf96b285e7128ddc1
RoseDuf <rose.dufresne@outlook.com>
2021-02-04 22:52:37 -0400

    hgdyjf

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private int m_Speed;
+
+    private Animator m_Animator;
+    
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void HandleInput()
+    {
+
+    }
+}
