5c4939d0bf4e48c70f722e70a67f27f569760927
RoseDuf <rose.dufresne@outlook.com>
2021-02-07 10:18:05 -0400

    deleted a bunch of sprites

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,22 +4,109 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private int m_Speed;
+    // Movement variables
+    [SerializeField] private float m_Speed;
 
+    private Vector2 inputVector;
+    private Vector2 moveAmount;
+    private Vector2 smoothMoveVelocity;
+    private Vector2 initialScale;
+
+    private bool isMoving;
+
+    // Rigidbody variables
+    [SerializeField] private float m_JumpForce;
+    private Rigidbody2D m_Rigidbody;
+
+    // Ground Check variables
+    [SerializeField] private LayerMask m_WhatIsGround;
+    private const float GROUND_CHECK_RADIUS = 0.1f;
+    private List<ColliderCheck> m_GroundCheckList;
+    private bool isGrounded;
+
+    // Animation variables
     private Animator m_Animator;
-    
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        // Obtain ground check components and store in list
+        m_GroundCheckList = new List<ColliderCheck>();
+        ColliderCheck[] groundChecksArray = transform.GetComponentsInChildren<ColliderCheck>();
+        foreach (ColliderCheck g in groundChecksArray)
+        {
+            m_GroundCheckList.Add(g);
+        }
+    }
+
     void Start()
     {
+        initialScale = transform.localScale;
+        isMoving = false;
+
         m_Animator = GetComponent<Animator>();
     }
-    
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        CheckGrounded();
     }
 
+    void Update()
+    {
+        HandleInput();
+        Movement();
+    }
+
+    /// <summary>
+    /// Logic associated to button presses.
+    /// </summary>
     void HandleInput()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        isMoving = horizontal != 0;
 
+        inputVector = new Vector2(horizontal, 0f);
+
+        moveAmount = Vector2.SmoothDamp(moveAmount, inputVector, ref smoothMoveVelocity, 0.15f);
+    }
+
+    /// <summary>
+    /// Handles transform and/or rigidbody operations needed to move character.
+    /// </summary>
+    void Movement()
+    {
+        //transform.position += new Vector3(inputVector.x, 0f, 0f) * m_Speed * Time.deltaTime;
+        //transform.Translate(inputVector * m_Speed * Time.deltaTime);
+        m_Rigidbody.velocity = Vector2.SmoothDamp(m_Rigidbody.velocity, inputVector, ref smoothMoveVelocity, 0.15f);
+        FaceDirection();
+    }
+
+    /// <summary>
+    /// Adjusts player sprite's direction by rescaling.
+    /// </summary>
+    void FaceDirection()
+    {
+        if (isMoving)
+        {
+            transform.localScale = new Vector3(inputVector.x * initialScale.x, transform.localScale.y, 0);
+        }
+    }
+
+    /// <summary>
+    /// Performs the logic and returns a boolean - true if the player is on the ground, false otherwise.
+    /// </summary>
+    private void CheckGrounded()
+    {
+        foreach (ColliderCheck g in m_GroundCheckList)
+        {
+            if (g.CheckCollision(GROUND_CHECK_RADIUS, m_WhatIsGround, gameObject))
+            {
+                isGrounded = true;
+                return;
+            }
+        }
+        isGrounded = false;
     }
 }
