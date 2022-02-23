457933476d4561133af112a754ee27766541853c
RoseDuf <rose.dufresne@outlook.com>
2022-02-22 23:47:59 -0400

    adding files

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] private Sprite m_FullHeartSprite;
+    [SerializeField] private Sprite m_EmptyHeartSprite;
+
+    [SerializeField] private GameObject m_HeartPrefab;
+    [SerializeField] private int m_NumberOfHearts;
+    private List<GameObject> m_HeartsList;
+
+    //Need reference to player
+
+    private void Start()
+    {
+        //TODO: Instantiat Hearts
+    }
+
+    private void Update()
+    {
+        //TODO: Decrement hearts
+    }
+
+}
